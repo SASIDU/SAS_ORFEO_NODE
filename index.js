347be35e1743f0cb459514d1d65d7b72b516c968
IDU - TCI Software SAS <idu@grupotci.com.co>
2020-09-04 11:14:03 -0500

    cambios

---
+++ index.js
@@ -0,0 +1,89 @@
+var http = require('http')
+var express = require('express')
+var app = express()
+var fs = require('fs');
+
+const path = require('path');
+const toPdf = require('mso-pdf');
+
+
+/*
+const doc = require('docx-pdf');
+const CloudmersiveConvertApiClient = require('cloudmersive-convert-api-client');const libre = require('libreoffice-convert');
+const { promisify } = require('bluebird');
+let lib_convert = promisify(libre.convert)*/
+
+try {
+
+  app.use(express.static('public'));
+
+  app.get('/file_download', function (req, res) {
+
+    let url_file = req.query.url_file;
+    let name_file = req.query.name_file;
+    let extencion_file = req.query.extencion_file;
+    let save = req.query.save;
+
+    if (url_file != '' && name_file != '' && extencion_file == '.docx' || extencion_file == 'docx') {
+
+      var file = fs.createWriteStream("tmp.docx");
+
+      var request = http.get(url_file, function (response) {
+        if (response.statusCode === 200) {
+          
+          response.pipe(file);
+
+          const enterPath = path.join(__dirname, `/tmp.docx`);
+          const outputPath = path.join(__dirname, `/public/` + name_file + `.pdf`);
+
+          toPdf.convert(enterPath, outputPath, function (errors) {
+
+            if (errors) {
+              res.end(JSON.stringify({ info: 'faild' }, null, 3));
+            }
+
+            var file_return = __dirname + `/public/` + name_file + `.pdf`;
+
+            file.on('finish', function() {
+              file.close();  // close() is async, call cb after close completes.
+            });
+
+            var data = fs.readFileSync(file_return);
+            res.contentType("application/pdf");
+            res.send(data);
+
+          });
+
+        }else{
+          file.close();
+          fs.unlink(dest, () => {}); 
+          res.end(JSON.stringify({ info: 'File no found' }, null, 3));
+        }
+        // Add timeout.
+        request.setTimeout(12000, function () {
+          request.abort();
+        });
+
+      }).on('error', function(err) { // Handle errors
+        fs.unlink(dest); // Delete the file async. (But we don't check the result)
+        res.end(JSON.stringify({ info: 'faild' }, null, 3));
+      });
+
+    } else {
+
+      res.end(JSON.stringify({ info: 'faild' }, null, 3));
+
+    }
+
+  });
+
+  http.createServer(app).listen(8001, () => {
+    console.log('Server started at http://localhost:8001');
+  });
+
+
+} catch (error) {
+
+  res.end(JSON.stringify({ info: 'faild' }, null, 3));
+
+}
