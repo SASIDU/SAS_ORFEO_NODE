603790aab643d1510df9df66d6f93d5a8d60cce3
IDU - TCI Software SAS <idu@grupotci.com.co>
2020-09-24 11:14:16 -0500

    cambios

--- index.js
+++ index.js
@@ -10,6 +10,60 @@ try {
 
   app.use(express.static('public'));
 
+  app.get('/convert', function(req , res){
+
+    let url_file = req.query.url_file;
+    let name_file = req.query.name_file;
+
+    if(url_file != '' && url_file != null && name_file != '' && name_file != null){
+
+      var file = fs.createWriteStream( name_file + ".docx");
+
+      var request = http.get(url_file, function (response) {
+        if (response.statusCode === 200) {
+  
+          response.pipe(file);
+  
+          const enterPath = path.join(__dirname, `/` + name_file +  `.docx`);
+          const outputPath = path.join(__dirname, `/public/` + name_file + `.pdf`);
+  
+          toPdf.convert(enterPath, outputPath, function (errors) {
+  
+            if (errors) {
+              res.end(JSON.stringify({ info: 'faild' }, null, 3));
+            }
+  
+            file.close();
+            fs.unlinkSync(enterPath);
+
+            res.end('true');
+  
+          });
+  
+        } else {
+          file.close();
+          res.end(JSON.stringify({ info: 'File no found' }, null, 3));
+        }
+        // Add timeout.
+        request.setTimeout(12000, function () {
+          request.abort();
+        });
+  
+      }).on('error', function (err) { // Handle errors
+        file.close();
+        res.end(JSON.stringify({ info: 'Archivo no encontrando' }, null, 3));
+      });
+
+    } else {
+
+      res.end(JSON.stringify({ info: 'File no found' }, null, 3));
+
+    }
+
+  });
+
+
+
   app.get('/file_download', function (req, res) {
 
     let url_file = req.query.url_file;
@@ -23,7 +77,7 @@ try {
 
       var request = http.get(url_file, function (response) {
         if (response.statusCode === 200) {
-          
+
           response.pipe(file);
 
           const enterPath = path.join(__dirname, `/tmp.docx`);
@@ -37,7 +91,7 @@ try {
 
             var file_return = __dirname + `/public/` + name_file + `.pdf`;
 
-            file.on('finish', function() {
+            file.on('finish', function () {
               file.close();  // close() is async, call cb after close completes.
             });
 
@@ -47,7 +101,7 @@ try {
 
           });
 
-        }else{
+        } else {
           file.close();
           //fs.unlink('./tmp.docx', () => {}); 
           res.end(JSON.stringify({ info: 'File no found' }, null, 3));
@@ -57,8 +111,8 @@ try {
           request.abort();
         });
 
-      }).on('error', function(err) { // Handle errors
-		file.close();
+      }).on('error', function (err) { // Handle errors
+        file.close();
         //fs.unlink('tmp.docx'); // Delete the file async. (But we don't check the result)
         res.end(JSON.stringify({ info: 'Archivo no encontrando' }, null, 3));
       });
@@ -71,7 +125,7 @@ try {
 
   });
 
-  http.createServer(app).listen(8001, '192.168.253.104', () => {
+  http.createServer(app).listen(8001, () => {
     console.log('Server started at http://apps.grupotci.com.co:8001');
   });
 
